3eafb60b5df5660236e2cfcdaf6394b37658e077
Nithyanandan <nithyanandan.t@gmail.com>
2017-05-19 19:09:52 -0700

    Add function to convert any coordinates to HEALPIX
    From ff6de25 (parabolic-cylinder):
    =================================
    
    In scripts/simbeam_to_healpix.py,
    
    (*) Add function convert_to_healpix() to take arbitrary coordinates
    as input and interpolate to HEALPIX grid
    
    In prisim/examples/pbparms/simbeam_to_healpix.yaml,
    
    (*) Add parameters to process interpolation from arbitrary
    coordinates to HEAKPIX coordinates under multiple interpolation
    methods

--- scripts/simbeam_to_healpix.py
+++ scripts/simbeam_to_healpix.py
@@ -18,7 +18,6 @@ def read_FEKO(infile):
     theta_range = [0.0, 0.0]
     phi_range = [0.0, 0.0]
     with open(infile, 'r') as fileobj:
-        gaindB_channel = None
         for linenum,line in enumerate(fileobj.readlines()):
             words = line.split()
             if 'Frequency' in line:
@@ -45,6 +44,87 @@ def read_FEKO(infile):
     phi = NP.linspace(phi_list.min(), phi_list.max(), nphi)
     return (freqs, theta_list, phi_list, theta, phi, gaindB)
 
+def convert_to_healpix(theta, phi, gains, nside=32, interp_method='spline', gainunits='dB', angunits='radians'):
+    try:
+        theta, phi, gains
+    except NameError:
+        raise NameError('Inputs theta, phi and gains must be specified')
+    if not HP.isnsideok(nside):
+        raise ValueError('Specified nside invalid')
+    if not isinstance(interp_method, str):
+        raise TypeError('Input interp_method must be a string')
+    if interp_method not in ['spline', 'nearest', 'healpix']:
+        raise valueError('Input interp_method value specified is invalid')
+    if gains.shape == (theta.size, phi.size):
+        gridded = True
+    elif (gains.size == theta.size) and (gains.size == phi.size):
+        gridded = False
+    else:
+        raise ValueError('Inputs theta, phi and gains have incompatible dimensions')
+    
+    if angunits.lower() == 'degrees':
+        theta = NP.radians(theta)
+        phi = NP.radians(phi)
+
+    hmap = NP.empty(HP.nside2npix(nside))
+    wtsmap = NP.empty(HP.nside2npix(nside))
+    hmap.fill(NP.nan)
+    wtsmap.fill(NP.nan)
+    
+    if interp_method == 'spline':
+        if gainunits.lower() != 'db':
+            gains = 10.0 * NP.log10(gains)
+        # gains -= NP.amax(gains)
+        hpxtheta, hpxphi = HP.pix2ang(nside, NP.arange(HP.nside2npix(nside)))
+        if gridded:
+            interp_func = interpolate.RectBivariateSpline(theta, phi, gains)
+            hmap = interp_func.ev(hpxtheta, hpxphi)
+        else:
+            # interp_func = interpolate.interp2d(theta, phi, gains, kind='cubic')
+            # hmap = interp_func(hpxtheta, hpxphi)
+            hmap = interpolate.griddata(NP.hstack((theta.reshape(-1,1),phi.reshape(-1,1))), gains, NP.hstack((hpxtheta.reshape(-1,1),hpxphi.reshape(-1,1))), method='cubic')
+        hmap -= NP.nanmax(hmap)
+        if gainunits.lower() != 'db':
+            hmap = 10**(hmap/10)
+    else:
+        if gainunits.lower() == 'db':
+            gains = 10**(gains/10.0)
+        # gains /= NP.amax(gains)
+        if gridded:
+            phi_flattened, theta_flattened = NP.meshgrid(phi, theta)
+            theta_flattened = theta_flattened.flatten()
+            phi_flattened = phi_flattened.flatten()
+            gains = gains.flatten()
+        else:
+            theta_flattened = theta
+            phi_flattened = phi
+        if interp_method == 'healpix':
+            ngbrs, wts = HP.get_interp_weights(nside, theta_flattened, phi=phi_flattened)
+            hmap[NP.unique(ngbrs)] = 0.0
+            wtsmap[NP.unique(ngbrs)] = 0.0
+            for i in xrange(theta_flattened.size):
+                hmap[ngbrs[:,i]] += wts[:,i] * gains[i]
+                wtsmap[ngbrs[:,i]] += wts[:,i]
+        else: # nearest neighbour
+            nnpix = HP.ang2pix(nside, theta_flattened, phi_flattened)
+            hmap[NP.unique(nnpix)] = 0.0
+            wtsmap[NP.unique(nnpix)] = 0.0
+            for i in xrange(theta_flattened.size):
+                hmap[nnpix[i]] += gains[i]
+                wtsmap[nnpix[i]] += 1
+        ind_nan = NP.isnan(wtsmap)
+        other_nanind = wtsmap < 1e-12
+        ind_nan = ind_nan | other_nanind
+        wtsmap[ind_nan] = NP.nan
+        hmap /= wtsmap
+        hmap /= NP.nanmax(hmap)
+        if gainunits.lower() == 'db':
+            hmap = 10.0 * NP.log10(hmap)
+    ind_nan = NP.isnan(hmap)
+    hmap[ind_nan] = HP.UNSEEN
+
+    return hmap
+
 if __name__ == '__main__':
 
     ## Parse input arguments
@@ -66,8 +146,17 @@ if __name__ == '__main__':
     outdir = ioparms['outdir']
     outfmt = ioparms['outfmt']
     outfile = outdir + ioparms['outfile'] + outfmt.lower()
+    nside = parms['processing']['nside']
+    gainunits = parms['processing']['gainunits']
+    interp_method = parms['processing']['interp']
     
     if infmt.lower() == 'feko':
-        freqs, theta_list, phit_list, theta, phi, gaindB = read_FEKO(infile)
+        freqs, theta_list, phi_list, theta, phi, gaindB = read_FEKO(infile)
     
-    
+    hmap1 = convert_to_healpix(theta_list, phi_list, gaindB[0], interp_method='spline', gainunits=gainunits, angunits='degrees')
+    hmap2 = convert_to_healpix(theta, phi, gaindB[0].reshape(phi.size,theta.size).T, interp_method='spline', gainunits=gainunits, angunits='degrees')
+    hmap3 = convert_to_healpix(theta, phi, gaindB[0].reshape(phi.size,theta.size).T, interp_method=interp_method, gainunits=gainunits, angunits='degrees')
+    hmap4 = convert_to_healpix(theta_list, phi_list, gaindB[0], interp_method='healpix', gainunits=gainunits, angunits='degrees')
+    hmap5 = convert_to_healpix(theta_list, phi_list, gaindB[0], interp_method='nearest', gainunits=gainunits, angunits='degrees')
+
+    PDB.set_trace()
