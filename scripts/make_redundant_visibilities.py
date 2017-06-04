fb76b5bd519b986d50329392556aefc1560030bc
Nithyanandan <nithyanandan.t@gmail.com>
2017-06-04 05:04:22 -0700

    Update scripts/make_redundant_visibilities.py
    From cd1432f (measurement-group):
    ================================
    
    In scripts/make_redundant_visibilities.py,
    
    (*) Update routine to make redundant visibilities directly from the
    attribute blgroups in class InterferometerArray if parmsfile is not
    specified

--- scripts/make_redundant_visibilities.py
+++ scripts/make_redundant_visibilities.py
@@ -24,78 +24,80 @@ if __name__ == '__main__':
     misc_group.add_argument('-w', '--wait', dest='wait', action='store_true', help='Wait after run')
     
     args = vars(parser.parse_args())
-
-    simobj = RI.InterferometerArray(None, None, None, init_file=args['simfile'])
-
-    if args['parmsfile'] is not None:
-        parmsfile = args['parmsfile']
-    else:
-        parmsfile = simvis.simparms_file
-        
-    with open(parmsfile, 'r') as pfile:
-        parms = yaml.safe_load(pfile)
-
     outfile = args['outfile']
     wait_after_run = args['wait']
 
-    blinfo = RI.getBaselineInfo(parms)
-    bl = blinfo['bl']
-    blgroups = blinfo['groups']
-    bl_length = NP.sqrt(NP.sum(bl**2, axis=1))
+    simobj = RI.InterferometerArray(None, None, None, init_file=args['simfile'])
+    if args['parmsfile'] is not None:
+        parmsfile = args['parmsfile']
+        with open(parmsfile, 'r') as pfile:
+            parms = yaml.safe_load(pfile)
 
-    simbl = simobj.baselines
-    if simbl.shape[0] == bl.shape[0]:
-        simbll = NP.sqrt(NP.sum(simbl**2, axis=1))
-        simblo = NP.angle(simbl[:,0] + 1j * simbl[:,1], deg=True)
-        simblza = NP.degrees(NP.arccos(simbl[:,2] / simbll))
-        
-        simblstr = ['{0[0]:.2f}_{0[1]:.3f}_{0[2]:.3f}'.format(lo) for lo in zip(simbll,simblza,simblo)]
+        blinfo = RI.getBaselineInfo(parms)
+        bl = blinfo['bl']
+        blgroups = blinfo['groups']
+        bl_length = NP.sqrt(NP.sum(bl**2, axis=1))
     
-        inp_blo = NP.angle(bl[:,0] + 1j * bl[:,1], deg=True)
-        inp_blza = NP.degrees(NP.arccos(bl[:,2] / bl_length))
-        inp_blstr = ['{0[0]:.2f}_{0[1]:.3f}_{0[2]:.3f}'.format(lo) for lo in zip(bl_length,inp_blza,inp_blo)]
-
-        uniq_inp_blstr, inp_ind, inp_invind = NP.unique(inp_blstr, return_index=True, return_inverse=True)  ## if numpy.__version__ < 1.9.0
-        uniq_sim_blstr, sim_ind, sim_invind = NP.unique(simblstr, return_index=True, return_inverse=True)  ## if numpy.__version__ < 1.9.0
-        # uniq_inp_blstr, inp_ind, inp_invind, inp_frequency = NP.unique(inp_blstr, return_index=True, return_inverse=True, return_counts=True)  ## if numpy.__version__ >= 1.9.0
-        # uniq_sim_blstr, sim_ind, sim_invind, sim_frequency = NP.unique(simblstr, return_index=True, return_inverse=True, return_counts=True)  ## if numpy.__version__ >= 1.9.0
-
-        if simbl.shape[0] != uniq_sim_blstr.size:
-            raise ValueError('Non-redundant baselines already found in the simulations')
+        simbl = simobj.baselines
+        if simbl.shape[0] == bl.shape[0]:
+            simbll = NP.sqrt(NP.sum(simbl**2, axis=1))
+            simblo = NP.angle(simbl[:,0] + 1j * simbl[:,1], deg=True)
+            simblza = NP.degrees(NP.arccos(simbl[:,2] / simbll))
+            
+            simblstr = ['{0[0]:.2f}_{0[1]:.3f}_{0[2]:.3f}'.format(lo) for lo in zip(simbll,simblza,simblo)]
         
-        if not NP.array_equal(uniq_inp_blstr, uniq_sim_blstr):
+            inp_blo = NP.angle(bl[:,0] + 1j * bl[:,1], deg=True)
+            inp_blza = NP.degrees(NP.arccos(bl[:,2] / bl_length))
+            inp_blstr = ['{0[0]:.2f}_{0[1]:.3f}_{0[2]:.3f}'.format(lo) for lo in zip(bl_length,inp_blza,inp_blo)]
+    
+            uniq_inp_blstr, inp_ind, inp_invind = NP.unique(inp_blstr, return_index=True, return_inverse=True)  ## if numpy.__version__ < 1.9.0
+            uniq_sim_blstr, sim_ind, sim_invind = NP.unique(simblstr, return_index=True, return_inverse=True)  ## if numpy.__version__ < 1.9.0
+            # uniq_inp_blstr, inp_ind, inp_invind, inp_frequency = NP.unique(inp_blstr, return_index=True, return_inverse=True, return_counts=True)  ## if numpy.__version__ >= 1.9.0
+            # uniq_sim_blstr, sim_ind, sim_invind, sim_frequency = NP.unique(simblstr, return_index=True, return_inverse=True, return_counts=True)  ## if numpy.__version__ >= 1.9.0
+    
+            if simbl.shape[0] != uniq_sim_blstr.size:
+                raise ValueError('Non-redundant baselines already found in the simulations')
+            
+            if not NP.array_equal(uniq_inp_blstr, uniq_sim_blstr):
+                raise ValueError('Layout from input simulation parameters file do not match simulated data.')
+            simobj.duplicate_measurements(blgroups=blgroups)
+        else:
+            raise ValueError('Layout from input simulation parameters file do not match simulated data.')
+    else:
+        simobj.duplicate_measurements()
+
+    for outfmt in args['outfmt']:
+        if outfmt.lower() == 'hdf5':
+            simobj.save(outfile, fmt=outfmt, verbose=True, tabtype='BinTableHDU', npz=False, overwrite=True, uvfits_parms=None)
+        else:
             if args['parmsfile'] is None:
-                raise IOError('Layout from simulations do not match simulated data.')
-            else:
-                raise IOError('Layout from input simulation parameters file do not match simulated data.')
-
-        simobj.duplicate_measurements(blgroups)
-
-        for outfmt in args['outfmt']:
-            if outfmt.lower() == 'hdf5':
-                simobj.save(outfile, fmt=outfmt, verbose=True, tabtype='BinTableHDU', npz=False, overwrite=True, uvfits_parms=None)
-            else:
-                uvfits_parms = None
-                if parms['save_formats']['phase_center'] is None:
-                    phase_center = simobj.pointing_center[0,:].reshape(1,-1)
-                    phase_center_coords = simobj.pointing_coords
-                    if phase_center_coords == 'dircos':
-                        phase_center = GEOM.dircos2altaz(phase_center, units='degrees')
-                        phase_center_coords = 'altaz'
-                    if phase_center_coords == 'altaz':
-                        phase_center = GEOM.altaz2hadec(phase_center, simobj.latitude, units='degrees')
-                        phase_center_coords = 'hadec'
-                    if phase_center_coords == 'hadec':
-                        phase_center = NP.hstack((simobj.lst[0]-phase_center[0,0], phase_center[0,1]))
-                        phase_center_coords = 'radec'
-                    if phase_center_coords != 'radec':
-                        raise ValueError('Invalid phase center coordinate system')
-                        
-                    uvfits_ref_point = {'location': phase_center.reshape(1,-1), 'coords': 'radec'}
-                else:
-                    uvfits_ref_point = {'location': NP.asarray(parms['save_formats']['phase_center']).reshape(1,-1), 'coords': 'radec'}
-                uvfits_parms = {'ref_point': uvfits_ref_point, 'method': parms['save_formats']['uvfits_method']}
+                parmsfile = simobj.simparms_file
                 
-                simobj.write_uvfits(outfile, uvfits_parms=uvfits_parms, overwrite=True)
+            with open(parmsfile, 'r') as pfile:
+                parms = yaml.safe_load(pfile)
+            
+            uvfits_parms = None
+            if parms['save_formats']['phase_center'] is None:
+                phase_center = simobj.pointing_center[0,:].reshape(1,-1)
+                phase_center_coords = simobj.pointing_coords
+                if phase_center_coords == 'dircos':
+                    phase_center = GEOM.dircos2altaz(phase_center, units='degrees')
+                    phase_center_coords = 'altaz'
+                if phase_center_coords == 'altaz':
+                    phase_center = GEOM.altaz2hadec(phase_center, simobj.latitude, units='degrees')
+                    phase_center_coords = 'hadec'
+                if phase_center_coords == 'hadec':
+                    phase_center = NP.hstack((simobj.lst[0]-phase_center[0,0], phase_center[0,1]))
+                    phase_center_coords = 'radec'
+                if phase_center_coords != 'radec':
+                    raise ValueError('Invalid phase center coordinate system')
+                    
+                uvfits_ref_point = {'location': phase_center.reshape(1,-1), 'coords': 'radec'}
+            else:
+                uvfits_ref_point = {'location': NP.asarray(parms['save_formats']['phase_center']).reshape(1,-1), 'coords': 'radec'}
+            uvfits_parms = {'ref_point': uvfits_ref_point, 'method': parms['save_formats']['uvfits_method']}
+            
+            simobj.write_uvfits(outfile, uvfits_parms=uvfits_parms, overwrite=True)
+
     if wait_after_run:
         PDB.set_trace()
