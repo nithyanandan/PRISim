ffc1266137a50d4cd0d7afa3535e07e4b71563ab
Nithyanandan <nithyanandan.t@gmail.com>
2018-11-13 17:20:17 -0700

    Add backwards compatibility for pyuvdata versions
    From a147da6 (pyuvdata):
    =======================
    
    In setup.py,
    
    (*) Modify pyuvdata package version requirement to be >=1.1
    
    In prisim/interferometry.py,
    
    (*) Import pyuvdata
    (*) Update member function __init__() of class InterferometerData
    to be backwards compatible with versions of pyuvdata earlier than 1.3

--- prisim/interferometry.py
+++ prisim/interferometry.py
@@ -26,6 +26,7 @@ import prisim
 import baseline_delay_horizon as DLY
 import primary_beams as PB
 try:
+    import pyuvdata
     from pyuvdata import UVData
 except ImportError:
     uvdata_module_found = False
@@ -9084,7 +9085,14 @@ class InterferometerData(object):
         self.infodict['baseline_array'] = 2048 * (self.infodict['ant_2_array'] + 1) + (self.infodict['ant_1_array'] + 1) + 2**16
         self.infodict['freq_array'] = prisim_object.channels.reshape(self.infodict['Nspws'],-1)
         self.infodict['polarization_array'] = NP.asarray([-5]).reshape(self.infodict['Npols']) # stokes 1:4 (I,Q,U,V); circular -1:-4 (RR,LL,RL,LR); linear -5:-8 (XX,YY,XY,YX)
-        self.infodict['integration_time'] = prisim_object.t_acc[0] + NP.zeros(self.infodict['Nblts']) # Replicate to be of shape (Nblts,) to be Baseline-Dependent-Averaging compliant
+        if uvdata_module_found:
+            if LooseVersion(pyuvdata.__version__)>=LooseVersion('1.3.2'):
+                self.infodict['integration_time'] = prisim_object.t_acc[0] + NP.zeros(self.infodict['Nblts']) # Replicate to be of shape (Nblts,) to be Baseline-Dependent-Averaging compliant with pyuvdata >= v1.3.2
+            else:
+                self.infodict['integration_time'] = prisim_object.t_acc[0]
+        else:
+            self.infodict['integration_time'] = prisim_object.t_acc[0] + NP.zeros(self.infodict['Nblts'])
+
         self.infodict['channel_width'] = prisim_object.freq_resolution
 
         # ----- Observation information ------
