6c7b27642f6c87f6a67229975251ae42c04b2063
Nithyanandan Thyagarajan <nthyagar@griffin.aoc.nrao.edu>
2019-01-27 04:53:13 -0700

    Specify memory available parm in run_prisim.py
    From 9326e07 (ephemeris):
    ========================
    
    In prisim/examples/simparms/defaultparms.yaml,
    
    (*) Add parameter 'memavail' under 'processing' to indicate total
    memory available. It is appropriate for use in scripts that span
    multiple nodes
    
    In scripts/run_prisim.py,
    
    (*) Update to use value in 'memavail' parameter based on changes
    above

--- scripts/run_prisim.py
+++ scripts/run_prisim.py
@@ -199,7 +199,11 @@ if gradient_mode is not None:
     if gradient_mode.lower() != 'baseline':
         raise ValueError('Specified gradient_mode not supported currently')
 memuse = parms['processing']['memuse']
-memory_available = psutil.virtual_memory().available # in Bytes
+memory_available = parms['processing']['memavail']
+if memory_available is None:
+    memory_available = psutil.virtual_memory().available # in Bytes
+else:
+    memory_available *= 2**30 # GB to bytes
 if memuse is None:
     memuse = 0.9 * memory_available
 elif isinstance(memuse, (int,float)):
