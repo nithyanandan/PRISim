bc5041b9a233217243ecebf9a118c60c87529a30
Nithyanandan Thyagarajan <nthyagar@griffin.aoc.nrao.edu>
2019-10-25 17:49:35 -0600

    Update script to write closure phases to NPZ file
    From a3b9a25 (closure-adv):
    ==========================
    
    In scripts/write_PRISim_bispectrum_phase_to_npz.py,
    
    (*) Import prisim and set default path
    (*) Correct the script description and default path of input YAML file
    
    In prisim/examples/ioparms/model_bispectrum_phase_to_npz_parms.yaml,
    
    (*) Update input parameters with updated defaults and updated
    descriptions

--- scripts/write_PRISim_bispectrum_phase_to_npz.py
+++ scripts/write_PRISim_bispectrum_phase_to_npz.py
@@ -2,16 +2,19 @@
 
 import yaml, argparse
 import numpy as NP
+import prisim
 from prisim import bispectrum_phase as BSP
+
+prisim_path = prisim.__path__[0]+'/'
 
 if __name__ == '__main__':
 
     ## Parse input arguments
     
-    parser = argparse.ArgumentParser(description='Program to duplicate redundant baseline measurements')
+    parser = argparse.ArgumentParser(description='Program to extract bispectrum phases and save to output file for further processing')
     
     input_group = parser.add_argument_group('Input parameters', 'Input specifications')
-    input_group.add_argument('-i', '--infile', dest='infile', default='/data3/t_nithyanandan/codes/mine/python/projects/closure/model_bispectrum_phase_to_npz_parms.yaml', type=str, required=False, help='File specifying input parameters')
+    input_group.add_argument('-i', '--infile', dest='infile', default=prisim_path+'examples/ioparms/model_bispectrum_phase_to_npz_parms.yaml', type=str, required=False, help='File specifying input parameters')
 
     args = vars(parser.parse_args())
     
